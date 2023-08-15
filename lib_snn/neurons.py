1861e664ddc672c327df59bae3f5cf48e4d5f720
sspark <seong.sik.park@gmail.com>
2023-08-15 07:39:34 -0400

    230815 - reg_spike_cout_sc modify

--- lib_snn/neurons.py
+++ lib_snn/neurons.py
@@ -636,8 +636,9 @@ class Neuron(tf.keras.layers.Layer):
                     reduce_axis= [i for i in range(1,n_dim)]
 
                     #self.add_loss(conf.reg_spike_out_const * tf.reduce_mean(self.out * self.spike_count / tf.reduce_max(self.spike_count)))
-                    self.add_loss(conf.reg_spike_out_const
-                                  * tf.reduce_mean(self.out * tf.math.divide_no_nan(self.spike_count,tf.reduce_max(self.spike_count,axis=reduce_axis,keepdims=True))))
+                    sc_norm = tf.math.divide_no_nan(self.spike_count,tf.reduce_max(self.spike_count,axis=reduce_axis,keepdims=True))
+                    eps = 0.25
+                    self.add_loss(conf.reg_spike_out_const*tf.reduce_mean(self.out * (1-sc_norm+eps)))
                     #self.add_loss(conf.reg_spike_out_const * tf.reduce_mean(self.out * self.spike_count))
 
                     #out_ret = self.reg_spike_out_fn(out_ret)
