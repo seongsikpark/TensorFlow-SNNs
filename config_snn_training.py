07159c45eca72d9211af5cf1b265f6e99de5aeb9
sspark <seong.sik.park@gmail.com>
2024-01-08 16:55:01 +0900

    240108 - lr schedule None add

--- config_snn_training.py
+++ config_snn_training.py
@@ -9,7 +9,7 @@ import os
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
 #os.environ["CUDA_VISIBLE_DEVICES"]="0,2"
 #os.environ["CUDA_VISIBLE_DEVICES"]="4,7"
-os.environ["CUDA_VISIBLE_DEVICES"]="1"
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
 #os.environ["CUDA_VISIBLE_DEVICES"]="0,1,2,3"
 #os.environ["CUDA_VISIBLE_DEVICES"]="0,1,2,3,4,5,6,7"
 #os.environ["CUDA_VISIBLE_DEVICES"]="0,1,2,3"
@@ -45,6 +45,7 @@ conf = config.flags
 conf.learning_rate = 0.0001
 conf.lmb = 1.0E-4
 conf.optimizer = 'ADAM'
+conf.lr_schedule = None
 
 #
 #conf.train_epoch = 90
