9b42ecd863ab554360f6ccdaedc6a58d59acfb93
sspark <seong.sik.park@gmail.com>
2024-01-07 19:09:52 -0500

    240108 - cifar10_dvs.py - load - shuffle_file ture

--- config_snn_training.py
+++ config_snn_training.py
@@ -9,7 +9,7 @@ import os
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
 #os.environ["CUDA_VISIBLE_DEVICES"]="0,2"
 #os.environ["CUDA_VISIBLE_DEVICES"]="4,7"
-os.environ["CUDA_VISIBLE_DEVICES"]="0"
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
 #os.environ["CUDA_VISIBLE_DEVICES"]="0,1,2,3"
 #os.environ["CUDA_VISIBLE_DEVICES"]="0,1,2,3,4,5,6,7"
 #os.environ["CUDA_VISIBLE_DEVICES"]="0,1,2,3"
@@ -39,10 +39,12 @@ conf = config.flags
 #conf.name_model_load='./models/VGG16_AP_CIFAR100/ep-300_bat-100_opt-SGD_lr-STEP-1E-01_lmb-1E-04_sc_cm_ts-4_nc-R-R_nr-z'
 
 
-4
+#conf.root_model_load='./models_ckpt_WTA-SNN'
+
+#
 conf.learning_rate = 0.0001
-conf.lmb = 1.0E-5
-conf.optimizer='ADAM'
+conf.lmb = 1.0E-4
+conf.optimizer = 'ADAM'
 
 #
 #conf.train_epoch = 90
