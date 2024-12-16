2db86f392d935d2887a5ff99eb7dbed199658305
yechankang <kyccj@github.com>
2024-12-17 01:19:51 +0900

    spikformer proto

--- models/spikformer.py
+++ models/spikformer.py
@@ -4,6 +4,7 @@ import tensorflow.keras.layers as nn
 from tensorflow.keras import Model
 from tensorflow.keras.layers import Layer
 from tensorflow.keras import Sequential
+from tensorflow.keras.layers import LayerNormalization
 
 from tensorflow import einsum
 from einops import rearrange, repeat
@@ -30,12 +31,16 @@ def spikformer(
         conf,
         model_name,
         include_top=True,
+        patch_size=16,
         embed_dims=[64,128,256],
         num_heads=[1,2,4],
         classes=1000,
         mlp_ratios=[4,4,4],
         qkv_bias=False,
         qk_scale=None,
+        drop_rate=0.,
+        attn_drop_rate=0.,
+        drop_path_rate=0.,
         depths=[6,8,6],
         sr_ratios=[8,4,2],
         dataset_name=None,
@@ -106,106 +111,151 @@ def spikformer(
     tdbn_first_layer = conf.nn_mode=='SNN' and conf.input_spike_mode=='POISSON' and conf.tdbn
     tdbn = conf.nn_mode=='SNN' and conf.tdbn
 
-    def MLP(x, in_features, hidden_features=None, out_features=None)
-        out_features = out_features or in_features
-        hidden_features = hidden_features or in_features
-        T,B,N,C = tf.shape(x)[0],tf.shape(x)[1], tf.shape(x)[2], tf.shape(x)[3]
-        x_ = tf.reshape(x, [T*B,N,C])
-        x = lib_snn.layers.Dense(hidden_features, name='fc1')
-        x = tf.transpose(x, perm=[0,2,1])
-        x = self.fc1_bn(x)
-        x = tf.transpose(x, perm=[0,2,1])
-        x = tf.reshape(x, [T,B,N, self.c_hidden])
-        x = self.fc1_lif(x)
 
-        x = tf.reshape(x, [T*B,N, self.c_hidden])
-        x = self.fc2_linear(x)
-        x = tf.transpose(x, perm=[0,2,1])
-        x = self.fc2_bn(x)
-        x = tf.transpose(x, perm=[0,2,1])
-        x = tf.reshape(x, [T,B,N,C])
-        x = self.fc2_lif(x)
+
+
+    def block(x, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
+              drop_path=0., norm_layer=LayerNormalization, sr_ratio=1):
+
+        def mlp(x, in_features, hidden_features=None, out_features=None):
+            out_features = out_features or in_features
+            hidden_features = hidden_features or in_features
+            T,B,N,C = tf.shape(x)[0],tf.shape(x)[1], tf.shape(x)[2], tf.shape(x)[3]
+            x_ = tf.reshape(x, [T*B,N,C])
+            x = lib_snn.layers.Dense(hidden_features,kernel_initializer=k_init,  name='MLP_fc1')(x_)
+            x = tf.transpose(x, perm=[0,2,1])
+            x = lib_snn.layers.BatchNormalization(en_tdbn=tdbn,name='MLP_bn_fc1')(x)
+            x = tf.transpose(x, perm=[0,2,1])
+            x = tf.reshape(x, [T, B, N, hidden_features])
+            x = lib_snn.activations.Activation(act_type=act_type,name='MLP_n_fc1')(x)
+
+            x = tf.reshape(x, [T*B,N,hidden_features])
+            x = lib_snn.layers.Dense(hidden_features,kernel_initializer=k_init, name='MLP_fc2')(x)
+            x = tf.transpose(x, perm=[0,2,1])
+            x = lib_snn.layers.BatchNormalization(en_tdbn=tdbn,name='MLP_bn_fc2')(x)
+            x = tf.transpose(x, perm=[0,2,1])
+            x = tf.reshape(x, [T, B, N, out_features])
+            x = lib_snn.activations.Activation(act_type=act_type,name='MLP_n_fc2')(x)
+
+            return x
+
+        def ssa(x, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0., sr_ratio=1):
+
+            assert dim % num_heads ==0, f"dim {dim} should be divisible by num_heads {num_heads}."
+            T, B, N, C = tf.shape(x)[0], tf.shape(x)[1], tf.shape(x)[2], tf.shape(x)[3]
+            scale = 0.125
+            x_for_qkv = tf.reshape(x, [T * B, N, C])
+
+            q_linear_out = lib_snn.layers.Dense(dim,kernel_initializer=k_init, name='q_fc1')(x_for_qkv)
+            q_linear_out = tf.transpose(q_linear_out, perm=[0, 2, 1])
+            q_linear_out = lib_snn.layers.BatchNormalization(en_tdbn=tdbn,name='q_bn1')(q_linear_out)
+            q_linear_out = tf.transpose(q_linear_out, perm=[0, 2, 1])
+            q_linear_out = tf.reshape(q_linear_out, [T, B, N, C])
+            q_linear_out = lib_snn.activations.Activation(act_type=act_type, name='ssa_q_lif1')(q_linear_out)
+            q = tf.reshape(q_linear_out, [T, B, N, num_heads, C // num_heads])
+            q = tf.transpose(q, perm=[0, 1, 3, 2, 4])  # Rearrange dimensions
+
+            k_linear_out = lib_snn.layers.Dense(dim,kernel_initializer=k_init, name='k_fc1')(x_for_qkv)
+            k_linear_out = tf.transpose(k_linear_out, perm=[0, 2, 1])
+            k_linear_out = lib_snn.layers.BatchNormalization(en_tdbn=tdbn,name='q_bn1')(k_linear_out)
+            k_linear_out = tf.transpose(k_linear_out, perm=[0, 2, 1])
+            k_linear_out = tf.reshape(k_linear_out, [T, B, N, C])
+            k_linear_out = lib_snn.activations.Activation(act_type=act_type, name='ssa_q_lif1')(k_linear_out)
+            k = tf.reshape(k_linear_out, [T, B, N, num_heads, C // num_heads])
+            k = tf.transpose(k, perm=[0, 1, 3, 2, 4])
+
+            v_linear_out = lib_snn.layers.Dense(dim,kernel_initializer=k_init, name='v_fc1')(x_for_qkv)
+            v_linear_out = tf.transpose(v_linear_out, perm=[0, 2, 1])
+            v_linear_out = lib_snn.layers.BatchNormalization(en_tdbn=tdbn,name='v_bn1')(v_linear_out)
+            v_linear_out = tf.transpose(v_linear_out, perm=[0, 2, 1])
+            v_linear_out = tf.reshape(v_linear_out, [T, B, N, C])
+            v_linear_out = lib_snn.activations.Activation(act_type=act_type, name='ssa_v_lif1')(v_linear_out)
+            v = tf.reshape(v_linear_out, [T, B, N, num_heads, C // num_heads])
+            v = tf.transpose(v, perm=[0, 1, 3, 2, 4])
+
+            attn = tf.matmul(q, k, transpose_b=True) * scale
+            x = tf.matmul(attn, v)
+
+            x = tf.transpose(x, perm=[0, 1, 3, 2, 4])  # [T, B, N, num_heads, head_dim]
+            x = tf.reshape(x, [T, B, N, C])  # Combine head_dim and num_heads
+            x = lib_snn.activations.Activation(act_type=act_type, name='ssa_att_lif1')(x)
+
+            x = tf.reshape(x, [T * B, N, C])  # Flatten T and B axes
+            x = lib_snn.layers.Dense(dim,kernel_initializer=k_init, name='proj_fc1')(x)
+            x = tf.transpose(x, perm=[0, 2, 1])  # Transpose for BatchNorm
+            x = lib_snn.layers.BatchNormalization(en_tdbn=tdbn,name='proj_bn1')(x)
+            x = tf.transpose(x, perm=[0, 2, 1])
+            x = tf.reshape(x, [T, B, N, C])
+            x = lib_snn.activations.Activation(act_type=act_type, name='ssa_proj_lif1')(x)
+
+            return x
+        norm1 = norm_layer(axis=-1)
+        norm2 = norm_layer(axis=-1)
+
+        mlp_hidden_dim = int(dim * mlp_ratio)
+
+        attn_out = ssa(norm1(x), dim=dim, num_heads=num_heads, qkv_bias=qkv_bias,
+                       qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop, sr_ratio=sr_ratio)
+
+        x = x + attn_out
+
+        mlp_out = mlp(norm2(x), in_features=dim, hidden_features=mlp_hidden_dim, out_features=dim)
+        x = x + mlp_out
 
         return x
 
-    img_input = tf.keras.layers.Input(shape=input_shape, batch_size=batch_size)
+    def sps(x, input_shape, patch_size=4, embed_dims=256):
 
-    input = lib_snn.layers.InputGenLayer(name='in')(img_input)
-    if conf.nn_mode =='SNN':
-        input = lib_snn.activations.Activation(act_type=act_type,loc='IN',name='n_in')(input)
+        patch_size = (patch_size, patch_size) if isinstance(patch_size, int) else patch_size
+        H, W = input_shape[0] // patch_size[0], input_shape[1] // patch_size[1]
+        num_patches = H * W
 
-    sps_c1 = lib_snn.layers.Conv2D()
+        T, B, C, H, W = tf.shape(x)[0], tf.shape(x)[1], tf.shape(x)[2], tf.shape(x)[3], tf.shape(x)[4]
 
+        x = tf.reshape(x, [-1, H, W, C])  # T*B, H, W, C
+        syn_c1 = lib_snn.layers.Conv2D(embed_dims // 8, kernel_size=3, strides=1, padding='SAME', kernel_initializer=k_init, name='sps_conv1')(x)
+        norm_c1 = tf.reshape(lib_snn.layers.BatchNormalization(en_tdbn=tdbn,name='sps_bn1')(syn_c1), [T, B, -1, H, W])
+        a_c1 = tf.reshape(lib_snn.activations.Activation(act_type=act_type,name='sps_lif1')(norm_c1), [-1, H, W, embed_dims // 8])
 
-class MLP(lib_snn.model.Model):
-    def __init__(self,in_dim,hidden_dim,out_dim,drop=0.0):
-        super(MLP,self).__init__()
-        dim = out_dim or in_dim
-        hidden_dim = hidden_dim or in_dim
-        self.fc1_linear = lib_snn.layers.Dense(hidden_dim,name='fc1')
-        self.fc1_bn=lib_snn.layers.BatchNormalization(name='bn_fc1')
-        self.fc1_lif=lib_snn.activations.Activation(act_type='LIF',name='n_fc1')
+        syn_c2 = lib_snn.layers.Conv2D(embed_dims // 4, kernel_size=3, strides=1, padding='SAME', kernel_initializer=k_init, name='sps_conv2')(a_c1)
+        norm_c2 = tf.reshape(lib_snn.layers.BatchNormalization(tdbn,name='sps_bn2')(syn_c2), [T, B, -1, H, W])
+        a_c2 = tf.reshape(lib_snn.activations.Activation(act_type=act_type,name='sps_lif2')(norm_c2), [-1, H, W, embed_dims // 4])
 
-        self.fc2_linear = lib_snn.layers.Dense(dim,name='fc2')
-        self.fc2_bn=lib_snn.layers.BatchNormalization(name='bn_fc2')
-        self.fc2_lif=lib_snn.activations.Activation(act_type='LIF',name='n_fc2')
-        self.c_hidden = hidden_dim
-        self.c_output = dim
+        syn_c3 = lib_snn.layers.Conv2D(embed_dims // 2, kernel_size=3, strides=1, padding='SAME', kernel_initializer=k_init, name='sps_conv3')(a_c2)
+        bn_c3 = tf.reshape(lib_snn.layers.BatchNormalization(tdbn,name='sps_bn3')(syn_c3), [T, B, -1, H, W])
+        a_c3 = tf.reshape(lib_snn.activations.Activation(act_type=act_type,name='sps_lif3')(bn_c3), [-1, H, W, embed_dims // 2])
+        a_p_c3 = lib_snn.layers.MaxPool2D((2,2),(2,2),name='sps_conv3_p')(a_c3) # why strides have to be only (2,2)?
 
-    def call(self, x):
-        T,B,N,C = tf.shape(x)[0],tf.shape(x)[1], tf.shape(x)[2], tf.shape(x)[3]
+        syn_c4 = lib_snn.layers.Conv2D(embed_dims, kernel_size=3, strides=1, padding='SAME', kernel_initializer=k_init, name='sps_conv4')(a_p_c3)
+        bn_c4 = tf.reshape(lib_snn.layers.BatchNormalization(tdbn,name='sps_bn4')(syn_c4), [T, B, -1, H, W])
+        a_c4 = tf.reshape(lib_snn.activations.Activation(act_type=act_type,name='sps_lif4')(bn_c4), [-1, H, W, embed_dims // 2])
+        a_p_c4 = lib_snn.layers.MaxPool2D((2,2),(2,2),name='sps_conv3_p')(a_c4) # why strides have to be only (2,2)?
 
-class SSA(lib_snn.model.Model):
-    def __init__(self,dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0.0, proj_drop=0.0, sr_ratio=1):
-        super().__init__()
-        assert dim % num_heads == 0, f"dim {dim} should be divided by num_heads {num_heads} "
-        self.dim = dim
-        self.num_heads = num_heads
-        self.scale = 0.125
-        self.q_linear =  lib_snn.layers.Dense(dim,name='lin_q1')
-        self.q_bn = lib_snn.layers.BatchNormalization(name='bn_q1')
-        self.q_lif = lib_snn.activations.Activation(acy_type='LIF',name='lif_q1')
+        rpe_feat = tf.reshape(a_p_c4, [T, B, -1, H // 4, W // 4])
+        rpe_c1 = lib_snn.layers.Conv2D(embed_dims, kernel_size=3, strides=1, padding='SAME', kernel_initializer=k_init, name='sps_conv_rpe')(rpe_feat)
+        rpe_norm_c1= tf.reshape(lib_snn.layers.BatchNormalization(en_tdbn=tdbn,name='sps_bn_rpe')(rpe_c1), [T, B, -1, H // 4, W // 4])
+        rpe_a1 = lib_snn.activations.Activation(act_type=act_type, name='sps_lif_rpe')(rpe_norm_c1)
+        rpe_out = rpe_a1 + rpe_feat
 
-        self.k_linear =  lib_snn.layers.Dense(dim,name='lin_k1')
-        self.k_bn = lib_snn.layers.BatchNormalization(name='bn_k1')
-        self.k_lif = lib_snn.activations.Activation(acy_type='LIF',name='lif_k1')
+        x = tf.reshape(rpe_out, [T, B, -1, embed_dims])
+        x = tf.transpose(x, perm=[0, 1, 3, 2])
+        return x
 
-        self.v_linear =  lib_snn.layers.Dense(dim,name='lin_v1')
-        self.v_bn = lib_snn.layers.BatchNormalization(name='bn_v1')
-        self.v_lif = lib_snn.activations.Activation(acy_type='LIF',name='lif_v1')
+    input_tensor =tf.keras.layers.Input(shape=input_shape,batch_size=batch_size)
 
-        self.attn_Lif = lib_snn.activations.Activation(act_type='LIF',name='lif_attn')
-        self.proj_linear = lib_snn.layers.Dense(dim,name='lin_proj')
-        self.proj_bn = lib_snn.layers.BatchNormalization(name='bn_proj')
-        self.proj_Lif = lib_snn.activations.Activation(act_type='LIF',name='lif_proj')
+    x = sps(input_tensor, input_shape=input_shape, patch_size=patch_size,
+            embed_dims=embed_dims[-1])
 
-    def call(self,x):
-        T,B,N,C = tf.shape(x)[0],tf.shape(x)[1], tf.shape(x)[2], tf.shape(x)[3]
-        x_for_qkv= tf.reshape(x, [T*B,N,C])
+    for stage_idx, depth in enumerate(depths):
+        for _ in range(depth):
+            x = block(x, dim=embed_dims[stage_idx], num_heads=num_heads[stage_idx],
+                      mlp_ratio=mlp_ratios[stage_idx], qkv_bias=qkv_bias, qk_scale=qk_scale,
+                      drop=drop_rate, attn_drop=attn_drop_rate, drop_path=drop_path_rate,
+                      norm_layer=LayerNormalization, sr_ratio=sr_ratios[stage_idx])
 
-        q_lin_out = self.q_linear(x_for_qkv)
-        q_lin_out = tf.transpose(q_lin_out, perm=[0,2,1])
-        q_lin_out = self.q_bn(q_lin_out)
-        q_lin_out = tf.transpose(q_lin_out, perm=[0,2,1])
-        q_lin_out = tf.reshape(q_lin_out, [T, B, N, C])
-        q_lin_out = self.q_lif(q_lin_out)
-        q = tf.reshape(q_lin_out, [T,B,N, self.num_heads, C // self.num_heads])
-        q = tf.transpose(q, perm=[0, 1, 3, 2, 4])
+    x = tf.keras.layers.GlobalAveragePooling1D()(x)
 
-        k_lin_out = self.k_linear(x_for_qkv)
-        k_lin_out = tf.transpose(k_lin_out, perm=[0,2,1])
-        k_lin_out = self.k_bn(k_lin_out)
-        k_lin_out = tf.transpose(k_lin_out, perm=[0,2,1])
-        k_lin_out = tf.reshape(k_lin_out, [T, B, N, C])
-        k_lin_out = self.k_lif(k_lin_out)
-        k = tf.reshape(k_lin_out, [T,B,N, self.num_heads, C // self.num_heads])
-        k = tf.transpose(k, perm=[0, 1, 3, 2, 4])
-
-        v_lin_out = self.v_linear(x_for_qkv)
-        v_lin_out = tf.transpose(v_lin_out, perm=[0,2,1])
-        v_lin_out = self.v_bn(v_lin_out)
-        v_lin_out = tf.transpose(v_lin_out, perm=[0,2,1])
-        v_lin_out = tf.reshape(v_lin_out, [T, B, N, C])
-        v_lin_out = self.v_lif(v_lin_out)
-        v = tf.reshape(v_lin_out, [T,B,N, self.num_heads, C // self.num_heads])
-        v = tf.transpose(v, perm=[0, 1, 3, 2, 4])
+    output_tensor = lib_snn.layers.Dense(classes, last_layer=True, kernel_initializer=k_init,name='predictions')(x) if classes > 0 else x
+    a_p = lib_snn.activations.Activation(act_type='softmax',name='a_predictions')(output_tensor)
+    model = lib_snn.model.Model(input_tensor, a_p, batch_size, input_shape, classes, conf, name=model_name)
+    return model
