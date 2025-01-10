08cbce955c71361dd98d665c3cf1b8ec150a3ae7
sspark <seong.sik.park@gmail.com>
2025-01-10 06:50:01 -0500

    250110 - add warmup_target

--- lib_snn/model_builder.py
+++ lib_snn/model_builder.py
@@ -107,13 +107,14 @@ def model_builder(
     learning_rate = conf.learning_rate
     lr_schedule = conf.lr_schedule
     step_decay_epoch = conf.step_decay_epoch
+    warmup_target=1E-5
     warmup_epochs = 20
 
     if lr_schedule == 'COS':
         # learning_rate = tf.keras.optimizers.schedules.CosineDecay(learning_rate, train_steps_per_epoch * train_epoch)
         #learning_rate = tf.keras.optimizers.schedules.CosineDecay(learning_rate, train_steps_per_epoch * train_epoch, alpha=1e-4)
         learning_rate = lib_snn.optimizers.CosineDecay(learning_rate, train_steps_per_epoch * train_epoch, alpha=1e-4,
-                                                       warmup_target=learning_rate, warmup_steps=train_steps_per_epoch*warmup_epochs)
+                                                       warmup_target=warmup_target, warmup_steps=train_steps_per_epoch*warmup_epochs)
     elif lr_schedule == 'COSR':
         learning_rate = tf.keras.optimizers.schedules.CosineDecayRestarts(learning_rate, lr_schedule_first_decay_step)
     elif lr_schedule == 'STEP':
