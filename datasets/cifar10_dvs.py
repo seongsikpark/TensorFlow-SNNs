7b627dbc5adff9dfe68c91614ae7462be9e6eadf
mangyeong <5xhtm7@naver.com>
2025-02-14 10:08:16 +0900

    250214_mangyeong_ms_update

--- datasets/cifar10_dvs.py
+++ datasets/cifar10_dvs.py
@@ -2,12 +2,12 @@
 
 
 import tensorflow_datasets as tfds
-import events_tfds.events.cifar10_dvs
+# import events_tfds.events.cifar10_dvs
 #from events_tfds.vis.image import as_frames
 #from events_tfds.vis.image import as_frame
 from datasets.events.image import as_frames
 from datasets.events.image import as_frame
-from events_tfds.vis.anim import animate_frames
+# from events_tfds.vis.anim import animate_frames
 
 
 
